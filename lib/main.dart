ead7be8468f9dda82b3eeea33159237d48a70317
Sargius <Dr.Sargius@gmail.com>
2026-07-11 14:01:06 +0400

    - feat(EXL-9): add bottom navigation shell for books, authors and categories

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // lib/main.dart
 
-import 'package:ex_libris/presentation/books/view/book_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ex_libris/presentation/home/view/home_shell_screen.dart';
 
 void main() {
   runApp(
@@ -23,7 +23,7 @@ class ExLibrisApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.blue,
       ),
-      home: const  BookListScreen(), // CategoryListScreen(), // BookListScreen(), // AuthorListScreen(),
+      home: const  HomeShellScreen(),
     );
   }
 }
---
+++ lib/presentation/home/view/home_shell_screen.dart
@@ -0,0 +1,60 @@
+// lib/presentation/home/view/home_shell_screen.dart
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ex_libris/presentation/books/view/book_list_screen.dart';
+import 'package:ex_libris/presentation/authors/view/author_list_screen.dart';
+import 'package:ex_libris/presentation/categories/view/category_list_screen.dart';
+
+/// Root shell with bottom navigation for Books, Authors and Categories.
+class HomeShellScreen extends ConsumerStatefulWidget {
+  const HomeShellScreen({super.key});
+
+  @override
+  ConsumerState<HomeShellScreen> createState() => _HomeShellScreenState();
+}
+
+class _HomeShellScreenState extends ConsumerState<HomeShellScreen> {
+  int _currentIndex = 0;
+
+  static const _pages = [
+    BookListScreen(),
+    AuthorListScreen(),
+    CategoryListScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _currentIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.menu_book_outlined),
+            selectedIcon: Icon(Icons.menu_book),
+            label: 'Books',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            selectedIcon: Icon(Icons.person),
+            label: 'Authors',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.category_outlined),
+            selectedIcon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+        ],
+      ),
+    );
+  }
+}
