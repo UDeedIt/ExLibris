a7a837f8b138399dde0f240f15bc6f9c4518475e
Sargius <Dr.Sargius@gmail.com>
2026-07-11 12:40:16 +0400

    - return "BookListScreen()" to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // lib/main.dart
 
-import 'package:ex_libris/presentation/categories/view/category_list_screen.dart';
+import 'package:ex_libris/presentation/books/view/book_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -23,7 +23,7 @@ class ExLibrisApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.blue,
       ),
-      home: const CategoryListScreen(), // BookListScreen(), // AuthorListScreen(),
+      home: const  BookListScreen(), // CategoryListScreen(), // BookListScreen(), // AuthorListScreen(),
     );
   }
 }
